099cf5bd062110126bb06b65ebeb084242df603e
Tambet Matiisen <tambet.matiisen@gmail.com>
2017-01-03 14:47:26 -0800

    Removed time limits from Gym env definitions, because Malmo has its own timeout and Gym's automatic reset doesn't really work when Malmo is not yet convinced that mission is over. Default Gym time limit seems to be long enough to accommodate all envs.

--- gym_minecraft/__init__.py
+++ gym_minecraft/__init__.py
@@ -8,7 +8,7 @@ register(
     id='MinecraftDefaultWorld1-v0',
     entry_point='gym_minecraft.envs:MinecraftEnv',
     kwargs={'mission_file': 'default_world_1.xml'},
-    tags={'wrapper_config.TimeLimit.max_episode_steps': 6060},
+    #tags={'wrapper_config.TimeLimit.max_episode_steps': 6060},
     #timestep_limit=6060,
     reward_threshold=1000
 )
@@ -17,7 +17,7 @@ register(
     id='MinecraftDefaultFlat1-v0',
     entry_point='gym_minecraft.envs:MinecraftEnv',
     kwargs={'mission_file': 'default_flat_1.xml'},
-    tags={'wrapper_config.TimeLimit.max_episode_steps': 2020},
+    #tags={'wrapper_config.TimeLimit.max_episode_steps': 2020},
     #timestep_limit=2020,
     reward_threshold=100
 )
@@ -26,7 +26,7 @@ register(
     id='MinecraftTrickyArena1-v0',
     entry_point='gym_minecraft.envs:MinecraftEnv',
     kwargs={'mission_file': 'tricky_arena_1.xml'},
-    tags={'wrapper_config.TimeLimit.max_episode_steps': 303},
+    #tags={'wrapper_config.TimeLimit.max_episode_steps': 303},
     #timestep_limit=303,
     reward_threshold=300
 )
@@ -35,7 +35,7 @@ register(
     id='MinecraftEating1-v0',
     entry_point='gym_minecraft.envs:MinecraftEnv',
     kwargs={'mission_file': 'eating_1.xml'},
-    tags={'wrapper_config.TimeLimit.max_episode_steps': 303},
+    #tags={'wrapper_config.TimeLimit.max_episode_steps': 303},
     #timestep_limit=303,
     reward_threshold=70
 )
@@ -44,7 +44,7 @@ register(
     id='MinecraftCliffWalking1-v0',
     entry_point='gym_minecraft.envs:MinecraftEnv',
     kwargs={'mission_file': 'cliff_walking_1.xml'},
-    tags={'wrapper_config.TimeLimit.max_episode_steps': 404},
+    #tags={'wrapper_config.TimeLimit.max_episode_steps': 404},
     #timestep_limit=404,
     reward_threshold=100
 )
@@ -53,7 +53,7 @@ register(
     id='MinecraftMaze1-v0',
     entry_point='gym_minecraft.envs:MinecraftEnv',
     kwargs={'mission_file': 'maze_1.xml'},
-    tags={'wrapper_config.TimeLimit.max_episode_steps': 606},
+    #tags={'wrapper_config.TimeLimit.max_episode_steps': 606},
     #timestep_limit=606,
     reward_threshold=1000
 )
@@ -62,7 +62,7 @@ register(
     id='MinecraftMaze2-v0',
     entry_point='gym_minecraft.envs:MinecraftEnv',
     kwargs={'mission_file': 'maze_2.xml'},
-    tags={'wrapper_config.TimeLimit.max_episode_steps': 2020},
+    #tags={'wrapper_config.TimeLimit.max_episode_steps': 2020},
     #timestep_limit=2020,
     reward_threshold=1000
 )
@@ -71,7 +71,7 @@ register(
     id='MinecraftBasic-v0',
     entry_point='gym_minecraft.envs:MinecraftEnv',
     kwargs={'mission_file': 'basic.xml'},
-    tags={'wrapper_config.TimeLimit.max_episode_steps': 606},
+    #tags={'wrapper_config.TimeLimit.max_episode_steps': 606},
     #timestep_limit=606,
     reward_threshold=980
 )
@@ -80,7 +80,7 @@ register(
     id='MinecraftObstacles-v0',
     entry_point='gym_minecraft.envs:MinecraftEnv',
     kwargs={'mission_file': 'obstacles.xml'},
-    tags={'wrapper_config.TimeLimit.max_episode_steps': 909},
+    #tags={'wrapper_config.TimeLimit.max_episode_steps': 909},
     #timestep_limit=909,
     reward_threshold=2000
 )
@@ -89,7 +89,7 @@ register(
     id='MinecraftSimpleRoomMaze-v0',
     entry_point='gym_minecraft.envs:MinecraftEnv',
     kwargs={'mission_file': 'simpleRoomMaze.xml'},
-    tags={'wrapper_config.TimeLimit.max_episode_steps': 909},
+    #tags={'wrapper_config.TimeLimit.max_episode_steps': 909},
     #timestep_limit=909,
     reward_threshold=4000
 )
@@ -98,7 +98,7 @@ register(
     id='MinecraftAttic-v0',
     entry_point='gym_minecraft.envs:MinecraftEnv',
     kwargs={'mission_file': 'attic.xml'},
-    tags={'wrapper_config.TimeLimit.max_episode_steps': 606},
+    #tags={'wrapper_config.TimeLimit.max_episode_steps': 606},
     #timestep_limit=606,
     reward_threshold=1000
 )
@@ -107,7 +107,7 @@ register(
     id='MinecraftVertical-v0',
     entry_point='gym_minecraft.envs:MinecraftEnv',
     kwargs={'mission_file': 'vertical.xml'},
-    tags={'wrapper_config.TimeLimit.max_episode_steps': 909},
+    #tags={'wrapper_config.TimeLimit.max_episode_steps': 909},
     #timestep_limit=909,
     reward_threshold=8000
 )
@@ -116,7 +116,7 @@ register(
     id='MinecraftComplexityUsage-v0',
     entry_point='gym_minecraft.envs:MinecraftEnv',
     kwargs={'mission_file': 'complexity_usage.xml'},
-    tags={'wrapper_config.TimeLimit.max_episode_steps': 606},
+    #tags={'wrapper_config.TimeLimit.max_episode_steps': 606},
     #timestep_limit=606,
     reward_threshold=1000
 )
@@ -125,7 +125,7 @@ register(
     id='MinecraftMedium-v0',
     entry_point='gym_minecraft.envs:MinecraftEnv',
     kwargs={'mission_file': 'medium.xml'},
-    tags={'wrapper_config.TimeLimit.max_episode_steps': 1818},
+    #tags={'wrapper_config.TimeLimit.max_episode_steps': 1818},
     #timestep_limit=1818,
     reward_threshold=16000
 )
@@ -134,7 +134,7 @@ register(
     id='MinecraftHard-v0',
     entry_point='gym_minecraft.envs:MinecraftEnv',
     kwargs={'mission_file': 'hard.xml'},
-    tags={'wrapper_config.TimeLimit.max_episode_steps': 2424},
+    #tags={'wrapper_config.TimeLimit.max_episode_steps': 2424},
     #timestep_limit=2424,
     reward_threshold=32000
 )
